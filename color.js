6825c74498a284dba564fe7686b9ddf5f04fea67
stella-four321 <stefour321@gatech.edu>
2022-03-21 18:04:27 -0400

    update

---
+++ color.js
@@ -0,0 +1,10 @@
+function counterFunction() {
+    let counter = 0;
+    
+    if(counter >= 1){
+        alert("Once a day. Don't be greety.");
+    }
+
+    counter ++;
+
+}
--- randomGen.js
+++ randomGen.js
@@ -1,48 +1,60 @@
-function myFunction(){
-    let x = Math.floor((Math.random() * 10) + 1);
-    let y = Math.floor((Math.random() * 100) + 1);
+function myFunction(){    
 
-    switch(x){
-        case 1:
-            document.getElementById('demo1').innerHTML = "Today is your day!";
-            document.getElementById('demo2').innerHTML = "Your lucky number is: " + y;
-            break;
-        case 2:
-            document.getElementById('demo1').innerHTML = "Be aware of the sky.";
-            document.getElementById('demo2').innerHTML = "Your lucky number is: " + y;
-            break;
-        case 3:
-            document.getElementById('demo1').innerHTML = "Your Phone might Die...";
-            document.getElementById('demo2').innerHTML = "Your lucky number is: " + y;
-            break;
-        case 4:
-            document.getElementById('demo1').innerHTML = "Music will brighten your day.";
-            document.getElementById('demo2').innerHTML = "Your lucky number is: " + y;
-            break;
-        case 5:
-            document.getElementById('demo1').innerHTML = "The rumor is true :O";
-            document.getElementById('demo2').innerHTML = "Your lucky number is: " + y;
-            break;
-        case 6:
-            document.getElementById('demo1').innerHTML = "Take care of your special person <br/>today.";
-            document.getElementById('demo2').innerHTML = "Your lucky number is: " + y;
-            break;
-        case 7:
-            document.getElementById('demo1').innerHTML = "Stop laying on your bed";
-            document.getElementById('demo2').innerHTML = "Your lucky number is: " + y;
-            break;
-        case 8:
-            document.getElementById('demo1').innerHTML = "You're hungry, make yourself <br/>some food.";
-            document.getElementById('demo2').innerHTML = "Your lucky number is: " + y;
-            break;
-        case 9:
-            document.getElementById('demo1').innerHTML = "Stop being so dramatic";
-            document.getElementById('demo2').innerHTML = "Your lucky number is: " + y;
-            break;
-        case 10:
-            document.getElementById('demo1').innerHTML = "The love is strong";
-            document.getElementById('demo2').innerHTML = "Your lucky number is: " + y;
-            break;
+    //Generate Random Color
+    const setBg = () => {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        document.body.style.backgroundColor = "#" + randomColor;
+      }
+      genNew.addEventListener("click", setBg);
+      setBg();
+      
+    const setquote = () => {
+        const randomInt = Math.floor((Math.random() * 20) + 1);
+        document.getElementById('quote').innerHTML = myquote(randomInt);
+    }
+    genNew.addEventListener("click", setquote);
+    setquote();
+
+    const setNum = () => {
+        const ranLuckyNum = Math.floor((Math.random() * 100) + 1);
+        document.getElementById('luckyNumber').innerHTML = "Your lucky number is: " + ranLuckyNum;
+    }
+    genNew.addEventListener("click", setNum);
+    setNum();
+
+
+    function myquote(myRandomInt) {
+        switch(myRandomInt){
+            case 1: return "Today is your day!";
+            case 2: return "Be aware of the sky.";
+            case 3: return "Your phone might Die...";
+            case 4: return "Music will brighten your <br/>day.";
+            case 5: return "The rumor is true :O";
+            case 6: return "Take care of your special <br/>person today.";
+            case 7: return "Stop laying on your bed";
+            case 8: return "You're hungry, make <br/>yourself some food.";
+            case 9: return "Stop being so dramatic";
+            case 10: return "The love is strong";
+            case 11: return "You'll fall today, but you <br/>can still walk. So don't worry";
+            case 12: return "What you just did, you <br/>did fine.";
+            case 13: return "Don't assume what <br/>you don't know";
+            case 14: return "CHILLAX!";
+            case 15: return "You're thinking too much. <br/>People don't care about you";
+            case 16: return "Don't be scared when you look <br/>into the mirror today";
+            case 17: return "People are looking at you."
+            case 18: return "It's nice out. Go for <br/>a walk."
+            case 19: return "Stop day dreaming. <br/>Start doing."
+            case 20: return "Cats are cute."
+            case 21: return
+            case 22: return
+        }
+        
     }
 
+    
+
+    //Switch statement for quote & lucky number
+   
+
   }
+  
